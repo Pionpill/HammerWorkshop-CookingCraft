3cd72af2de73b9bec27a4d8a5c935f99ad7d54d0
Pionpill <673486387@qq.com>
2022-07-21 23:19:57 +0800

    ..

--- CookingCraft/BehaviorPack/hammerCookingScripts/common/factory/PlantsFactory.py
+++ CookingCraft/BehaviorPack/hammerCookingScripts/common/factory/PlantsFactory.py
@@ -4,7 +4,7 @@ version: 1.0
 Author: Pionpill
 LastEditors: Pionpill
 Date: 2022-07-11 22:15:04
-LastEditTime: 2022-07-17 00:18:01
+LastEditTime: 2022-07-19 23:17:39
 '''
 from hammerCookingScripts.common.proxy import PlantProxy
 from hammerCookingScripts.common.entity import Plant
@@ -16,14 +16,6 @@ class PlantsFactory(object):
 
     def __init__(self):
         object.__init__(self)
-
-    @classmethod
-    def GetPlantEntity(cls, seedName):
-        # type: (str) -> Plant
-        """获取 Plant 类，实例存储在字典中"""
-        if not cls.PlantEntityDict.get(seedName, None):
-            cls.PlantEntityDict[seedName] = Plant(seedName)
-        return cls.PlantEntityDict.get(seedName)
 
     @classmethod
     def GetPlantProxy(cls, seedName):
